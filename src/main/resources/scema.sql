5ba774e424bdb91fef5b164290395d2ffea15615
ahsel21 <sik63rus@mail.ru>
2021-09-08 22:48:14 +0400

    some fix

--- src/main/resources/scema.sql
+++ src/main/resources/scema.sql
@@ -1,8 +1,26 @@
-CREATE TABLE Client
+CREATE TABLE CLIENT
 (
-    Id INT,
-    FullName VARCHAR(70),
+    ClientId    BIGINT NOT NULL PRIMARY KEY,
+    FullName    VARCHAR(70),
     PhoneNumber VARCHAR(11),
-    Email VARCHAR(40),
-    passportId VARCHAR(6)
+    Email       VARCHAR(40),
+    PassportId  VARCHAR(6)
 );
+CREATE TABLE CREDIT
+(
+    CREDITID     BIGINT NOT NULL PRIMARY KEY,
+    LIMIT        BIGINT,
+    INTERESTRATE DOUBLE,
+    NAME         VARCHAR(50)
+);
+CREATE TABLE CREDITOFFER
+(
+    CREDITOFFERID BIGINT NOT NULL PRIMARY KEY,
+    CLIENTID      BIGINT,
+    CREDITID      BIGINT,
+    BANKID        BIGINT,
+    AMOUNT           BIGINT,
+    MONTHCOUNT      BIGINT
+
+);
+
